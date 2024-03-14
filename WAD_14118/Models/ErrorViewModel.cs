021b935c6c7e4e625a902b1aee9d0bd308f3e1b5
eo <eo@HOME-PC>
2024-03-14 11:42:06 +0500

    Add project files.

---
+++ WAD_14118/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WAD_14118.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
