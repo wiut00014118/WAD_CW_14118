498e1484728903e00951fe5ec2895be0a2070459
00014118 <eo@HOME-PC>
2024-03-14 12:18:27 +0500

    first created classes

---
+++ WAD_14118/Models/Genre.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WAD_14118.Models
+{
+    public enum Genre
+    {
+        Drama,
+        Comedy,
+        Romance,
+        [Display(Name = "Romantic Comedy")]
+        RomCom,
+        Crime,
+        Mystery
+    }
+}
---
+++ WAD_14118/Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿namespace WAD_14118.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public decimal Rating { get; set; }
+
+        public string ImdbUrl { get; set; }
+    }
+}
